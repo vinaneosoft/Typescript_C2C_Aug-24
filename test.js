6810fe6f1798390e1c5fb331c298067003cbd12a
vinaneosoft <vinaneosoft@gmail.com>
2024-08-08 15:50:17 +0530

    Initial commit

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log("Hi");
+console.log("Hi");
+console.log("Hi");
+console.log("Hi");
+// typescript is superset of javascript
+// any javascript logic u can write in typescript
+// we can not write typescript logic in javascript
