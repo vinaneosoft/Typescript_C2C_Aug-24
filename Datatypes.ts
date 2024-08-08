aa1fcd0649d0c01ec863ccd7954d5c528cd2ce5e
vinaneosoft <vinaneosoft@gmail.com>
2024-08-08 17:12:39 +0530

    commit

--- Datatypes.ts
+++ Datatypes.ts
@@ -5,7 +5,7 @@ cname=true
 
 let address="fhdjhdghkfjhj"; // strict
 
-let lname:string; // strict
+let lname:string; // strict   type annotations
 lname="pawar";  // try to assign incompatible value
 
 let lastname:String;
@@ -27,5 +27,22 @@ let marriedStatus:Boolean;
 marriedStatus=new Boolean(married); // wrapping
 // make unwrapping
 
+let xyz=89; // number
 
 // named function, annonymous function, arrow function
+function test1():number  // type annotations
+{
+    return 10;
+}
+
+function test2():void{
+   // return 10;
+}
+
+const multiply = function (num1:number, num2:number) : number{
+    return num1 * num2;
+}
+
+const add = (a: number, b: number): number => a + b;
+
+const upperstring = (str:string): string => str.toUpperCase();
